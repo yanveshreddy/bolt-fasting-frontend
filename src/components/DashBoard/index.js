fc47d569a14e10c09d0887aa5f450179e93e9b85
yanveshreddy <anvesh.learnings@gmail.com>
2021-05-17 18:48:43 +0530

    Dashboard layout structure

--- src/components/DashBoard/index.js
+++ src/components/DashBoard/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./index.css";
 
+import WeeklyFastingGraph from "../WeeklyFastingGraph";
+import Timer from "../Timer";
+import StatContainer from "../StatContainer";
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard works</h1>
+    <div className="container bg-container mt-5">
+      <div className="row">
+        <div className="col-12 col-md-4 mb-5">
+          <Timer />
+        </div>
+        <div className="col-12 col-md-8 mb-5">
+          <WeeklyFastingGraph />
+        </div>
+        <div className="col-12">
+          <StatContainer />
+        </div>
+      </div>
     </div>
   );
 };
