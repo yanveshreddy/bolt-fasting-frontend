1d110876c648546931fad279f8599eed8a3d69a3
yanveshreddy <anvesh.learnings@gmail.com>
2021-05-19 17:00:51 +0530

    styling property changes

--- src/components/WeeklyFastingGraph/index.js
+++ src/components/WeeklyFastingGraph/index.js
@@ -1,41 +1,31 @@
-// import React from "react";
-// import "./index.css";
-
-// const WeeklyFastingGraph = () => {
-//   return <div className="bg-weekly-graph "></div>;
-// };
-
-// export default WeeklyFastingGraph;
-
 import React, { useState } from "react";
-// import * as chart from "chart.js";
+import "./index.css";
 import { Bar } from "react-chartjs-2";
+
 // const randomInt = () => Math.floor(Math.random() * (10 - 1 + 1)) + 1;
 
 const WeeklyFastingGraph = () => {
-  // const [barData, setBarData] = useState({});
-
-  //   // set options
-  //   const [barOptions, setBarOptions] = useState({});
   const [barData, setBarData] = useState({
     labels: ["label 1", "label 2", "label 3", "label 4", "label 5", "label 6"],
     datasets: [
       {
         label: "test label",
-        // barPercentage: 0.6,
+        barPercentage: 0.6,
         barThickness: 8,
         // maxBarThickness: 6,
         // minBarLength: 2,
-        data: [48, 35, 73, 82, 64, 35],
+        data: [0, 16, 20, 14, 12, 18],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.6)",
-          "rgba(54, 162, 235, 0.6)",
-          "rgba(255, 206, 86, 0.6)",
-          "rgba(75, 192, 192, 0.6)",
-          "rgba(255, 99, 132, 0.6)",
-          "rgba(54, 162, 235, 0.6)",
+          "#5DD362",
+          "#A3A3A3",
+          "#42BC53",
+          "#A3A3A3",
+          "#EDB98A",
         ],
-        borderWidth: 3,
+        borderWidth: 0,
+        borderRadius: 10,
+        pointStyle: "circle",
+        drawtick: false,
       },
     ],
   });
@@ -45,19 +35,21 @@ const WeeklyFastingGraph = () => {
       scales: {
         xAxes: [
           {
-            barPercentage: 0.5,
+            barPercentage: 0.4,
           },
-        ],
-        yAxes: [
           {
-            ticks: {
-              beginAtZero: true,
-              min: 0,
-              max: 20,
-              stepSize: 5,
-            },
+            linewidth: 0,
           },
         ],
+        y: {
+          max: 24,
+          min: 0,
+          ticks: {
+            stepSize: 4,
+          },
+
+          linewidth: 0.1,
+        },
       },
       title: {
         display: true,
@@ -72,7 +64,7 @@ const WeeklyFastingGraph = () => {
   });
 
   return (
-    <div>
+    <div className="bg-weekly-graph">
       <Bar data={barData} options={barOptions.options} />
     </div>
   );
